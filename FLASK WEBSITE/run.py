baae42c38554c80be1634bdf7facbbe1573af438
IvanDS <ivan15_m_a@hotmail.com>
2020-09-30 12:35:55 +0200

    include path of directory

--- FLASK WEBSITE/run.py
+++ FLASK WEBSITE/run.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python
+import sys
+from os.path import dirname
+sys.path.append(dirname(__file__))
 
-from flask_website_dashboard import app
+from flask_website_dashboard.views import app
 
 if __name__ == "__main__":
     app.run(debug=True)
